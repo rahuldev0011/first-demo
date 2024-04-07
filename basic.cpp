e65dddd942f2b8bbc717d712512012fc26d18880
RAHUL DEV <155677377+rahuldev0011@users.noreply.github.com>
2024-04-07 10:16:15 +0530

    Add files via upload

---
+++ basic.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+// normal method
+// int main (){
+//     int n=10 ;
+//     for (int i=0;i<n;i++){
+//         for (int j=0;j<i;j++){
+//         cout<<"*";}
+//         cout <<endl;
+//     }
+// }
+
+//FUNCTION
+// void fun(){
+//     cout<<"good morning"<<endl;
+//     cout<<"have a nice day"<<endl;
+// }
+// int main(){
+//     //calling
+//     fun();
+//     fun();
+//     fun();
+//     fun();
+
+// }
+
+// triangle
+// void tri(int x){
+//     for(int i=0;i<=x;i++){
+//         for(int j=0;j<i;j++){
+//             cout<<"*";
+//         }cout<<endl;
+//     }
+// }
+// int main(){
+//     tri(3);
+//     cout<<"good";
+//     tri(4);
+// }
+
+//sum of ttwo 
+// int sum(int x, int y){
+//     return x+y;
+// }
+// int main(){
+//     // cout<<sum(3,4);
+//     int n,m;
+//     cin>>n>>m;
+//     //inbuilt function
+//     cout<<min(n,m);
+// }
+
